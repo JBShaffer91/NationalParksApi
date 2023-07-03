94fd7278d8f0d16e09ebee51c435cd42bf2af107
Justin Shaffer <justinbshaffer91@gmail.com>
2023-07-03 16:47:19 -0700

    Create ParkDbContext
    - Create Data directory
    - Create ParkDbContext.cs file
    - Configure ParkDbContext to interact with Park model and database
    Update Program.cs
    
    - Register ParkDbContext in ConfigureServices method
    - Configure database connection using connection string from appsettings.json

--- NationalParksApi/Program.cs
+++ NationalParksApi/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NationalParksApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ParkDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
